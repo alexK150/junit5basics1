dc6088cc8180bf00ae37225c008291fe982ecf7c
Alex <akuzin20@gmail.com>
2020-03-17 12:19:39 +0300

    commit v2 practice with BeforeAll, AfterAll, BeforeEach, AfterEach and small refactoring of code according to new tests

--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,16 +1,36 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAllTest() {
+        System.out.println("Before all tests");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Test started...");
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("Alex"));
+    }
+
+    @AfterEach
+    void afterTest() {
+        System.out.println("After each test...");
+    }
+
+    @AfterAll
+    static void afterAllTest() {
+        System.out.println("After all tests");
     }
 }
